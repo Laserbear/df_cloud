b759cfb64de99ebced8ad65e2a2a67c4e6ace228
laserbear <10689391+Laserbear@users.noreply.github.com>
2024-04-17 12:48:05 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='df_cloud',
+version='0.1.0',
+author='Christian Ng',
+author_email='10689391+Laserbear@users.noreply.github.com',
+description='A python module to upload dataframes to the cloud',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
