8cb55e740efa22abc74796abce2a872d2a25631f
laserbear <10689391+Laserbear@users.noreply.github.com>
2024-04-21 23:49:20 -0700

    add from_cloud function

--- df_cloud/df_cloud.py
+++ df_cloud/df_cloud.py
@@ -1,6 +1,15 @@
 import requests
-from pandas.core.base import PandasObject
-URL = "https://df-cloud.org"
+import pandas as pd
+
+URL = "https://df-cloud.org/"
 def to_cloud(df):
-    requests.post(URL, data=df.to_json)
-PandasObject.to_cloud = to_cloud
+    requests.post(URL+"upload", data=df.to_json)
+
+def from_cloud(json_url):
+    json = dict(request.get(json_url))
+    df = pd.DataFrame.from_dict(json, orient='index')
+    df.reset_index(level=0, inplace=True)
+    return df
+
+pd.core.base.PandasObject.to_cloud = to_cloud
+pd.DataFrame.from_cloud = from_cloud
